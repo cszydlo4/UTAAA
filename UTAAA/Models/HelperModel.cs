a333433e32adfeead223212af2d189d25d2e1cdb
Dante Scheele <dante_scheele@outlook.com>
2018-04-07 03:53:27 -0400

    Created Helper Class, Added db Connection String to web.config & placed login in seperate file, Added Dapper, removed boilerplate controllers/views.

---
+++ UTAAA/Models/HelperModel.cs
@@ -0,0 +1,18 @@
+﻿//Makes db connection string simpler
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace UTAAA.Models
+{
+    public static class HelperModel
+    {
+        public static string cnnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
